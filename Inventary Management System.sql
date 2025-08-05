194b61cf2de434a95204de45853f54034163b0d7
SyleshwaranS <syleshwarans@gmail.com>
2025-08-05 09:40:34 +0530

    Adding SQL file

---
+++ Inventary Management System.sql
@@ -0,0 +1,7 @@
+create database Inventary;
+
+use Inventary;
+
+create table product_stock(id int auto_increment primary key,name varchar(255),quantity int , price double );
+
+select * from product_stock;
