fe9a125298014a7a49a567289a17ec4247f8c5a9
SyleshwaranS <syleshwarans@gmail.com>
2025-08-05 09:49:54 +0530

    Update Inventary Management System.sql

--- Inventary Management System.sql
+++ Inventary Management System.sql
@@ -1,7 +1,9 @@
+-- creating new database named "Inventary".
 create database Inventary;
 
+-- selecting database to create table. 
 use Inventary;
 
+-- Creating table "product_stock" 
 create table product_stock(id int auto_increment primary key,name varchar(255),quantity int , price double );
-
-select * from product_stock;
+-- here id column is the primary column and auto increment 
